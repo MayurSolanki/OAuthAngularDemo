20dfa2128d1eebe45993832ade6f1ce796c6ad71
Mayur Solanki <mayursolanki120@gmail.com>
2024-04-28 20:32:44 +0530

    base href

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,10 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-  { path: 'oauthangulardemo', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'home', component: AppComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: '**', redirectTo: 'login'}//Redirect to login for any  invalid routes
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect to home if no route specified
+  { path: '**', redirectTo: '/login' } // Redirect to home for any other invalid routes
+
 ];
