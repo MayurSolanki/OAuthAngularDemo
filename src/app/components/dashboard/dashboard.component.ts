431647b491b659da5b4e36150df5bcd7dbfb2380
Mayur Solanki <mayursolanki120@gmail.com>
2024-04-29 16:54:03 +0530

    params received on  login

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -4,52 +4,42 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
-  oauthConsumerKeyy: string = "";
-  successCallBack: string = "";
   countdown: number = 10;
-  dataString = "";
+  dataString = '';
+  //
+  oauthConsumerKey: string = '';
+  successCallBack: string = '';
+  oauthConsumerSecret: string = '';
+  storeBaseUrl: string = '';
+  oauthVerifier: string = '';
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     // Extract credentials from URL parameters
-    this.route.queryParams.subscribe(params => {
-      console.log("params", params);
-      const oauthConsumerKey = params['oauth_consumer_key'];
-      const oauthConsumerSecret = params['oauth_consumer_secret'];
-      const storeBaseUrl = params['store_base_url'];
-      const oauthVerifier = params['oauth_verifier'];
-      // Process the received credentials as needed
-
-      this.oauthConsumerKeyy = params['oauth_consumer_keyy'];
+    this.route.queryParams.subscribe((params) => {
+      this.oauthConsumerKey = params['oauth_consumer_keyy'];
       this.successCallBack = params['success_call_back'];
+      this.oauthConsumerSecret = params['oauth_consumer_secret'];
+      this.storeBaseUrl = params['store_baseUrl'];
+      this.oauthVerifier = params['oauth_verifier'];
 
-      console.log("params", params);
-      console.log("oauthConsumerKey", oauthConsumerKey);
-      console.log("oauthConsumerSecret", oauthConsumerSecret);
-      console.log("storeBaseUrl", storeBaseUrl);
-      console.log("oauthVerifier", oauthVerifier);
-      console.log("oauthConsumerKeyy", this.oauthConsumerKeyy);
-      console.log("successCallBack", this.successCallBack);
 
-      this.dataString =
+       this.dataString =
       ` \n
       params : ${params} \n
-      oauthConsumerKey : ${oauthConsumerKey} \n
-      oauthConsumerSecret : ${oauthConsumerSecret} \n
-      storeBaseUrl : ${storeBaseUrl} \n
-      oauthVerifier : ${oauthVerifier} \n
-      oauthConsumerKeyy : ${this.oauthConsumerKeyy} \n
+      oauthConsumerKey : ${this.oauthConsumerKey} \n
+      oauthConsumerSecret : ${this.oauthConsumerSecret} \n
+      storeBaseUrl : ${this.storeBaseUrl} \n
+      oauthVerifier : ${this.oauthVerifier} \n
       successCallBack : ${this.successCallBack} \n
       `;
 
-    // Start the countdown timer
-    this.startCountdown();
-
+      // Start the countdown timer
+      this.startCountdown();
     });
   }
 
@@ -65,6 +55,4 @@ export class DashboardComponent implements OnInit {
       }
     }, 1000);
   }
-
-
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -10,6 +10,11 @@ export class LoginComponent implements OnInit {
 
    oauthConsumerKey: string = "";
    successCallBack: string = "";
+   oauthConsumerSecret:string = "";
+   storeBaseUrl:string = "";
+   oauthVerifier:string = "";
+
+
 
   constructor(private router: Router,private route: ActivatedRoute) { }
 
@@ -19,14 +24,35 @@ export class LoginComponent implements OnInit {
       // Read the oauth_consumer_key and success_call_back parameters
       this.oauthConsumerKey = params['oauth_consumer_key'];
       this.successCallBack = params['success_call_back'];
+      //
+      this.oauthConsumerSecret = params['oauth_consumer_secret'];
+      this.storeBaseUrl = params['store_base_url'];
+      this.oauthVerifier = params['oauth_verifier'];
+
+
+      console.log("oauthConsumerKey", this.oauthConsumerKey);
+      console.log("successCallBack", this.successCallBack);
+      console.log("oauthConsumerSecret", this.oauthConsumerSecret);
+      console.log("storeBaseUrl", this.storeBaseUrl);
+      console.log("oauthVerifier", this.oauthVerifier);
+
+
+
     });
   }
 
   OnClick(){
     this.router.navigate(['/dashboard'], {
       queryParams: {
-        oauth_consumer_keyy: this.oauthConsumerKey,
-        success_call_back: this.successCallBack
+        oauth_consumer_key: this.oauthConsumerKey,
+        success_call_back: this.successCallBack,
+        //
+        oauth_consumer_secret : this.oauthConsumerSecret,
+        store_baseUrl :  this.storeBaseUrl,
+        oauth_verifier : this.oauthVerifier,
+
+
+
       }
     });
   }
